eca3751af4999a1ad33f6366ccf7a2808ded3800
Sandip Sadhukhan <sandippakhanna@gmail.com>
2024-07-20 18:47:12 +0530

    promises part 2

--- 9.promises.js
+++ 9.promises.js
@@ -24,3 +24,143 @@
 
 
 // async/await
+// function importantAction(username) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(`Subscribe to ${username}`)
+//         }, 1000)
+//     })
+// }
+
+
+// function likeTheVideo(video) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(`Like the ${video} video`);
+//         }, 100)
+//     })
+// }
+
+// function shareTheVideo(video) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             reject(`Share the ${video} video`);
+//         }, 500)
+//     })
+// }
+
+// const result = async () => {
+//     try {
+//         const message1 = await importantAction("Sandip");
+//         const message2 = await likeTheVideo("Sandip");
+//         const message3 = await shareTheVideo("Sandip");
+//     } catch(error) {
+//         console.error("Promises failed", error)
+//     }
+// }
+
+// result();
+
+// -------
+// What is the output
+// console.log("start")
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log(1);
+//     resolve(2);
+// });
+
+// promise1.then(res => {
+//     console.log(res);
+// })
+
+// console.log("end");
+
+// Output
+// start
+// 1
+// end
+// 2
+
+// Reason -> Javascript engine run sync code first then async code.
+// So it will initialize promise's sync code, and run async code ie. promise.then later
+
+
+// What is the output
+// console.log("start")
+
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log(1);
+//     resolve(2);
+//     console.log(3);
+// })
+
+// promise1.then(res => {
+//     console.log(res);
+// })
+
+// console.log("end")
+
+/*
+Output
+======
+start
+1
+3
+end
+2
+*/
+
+
+// What is the output
+// console.log("start")
+
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log(1);
+//     console.log(3);
+// })
+
+// promise1.then(res => {
+//     console.log(res);
+// })
+
+// console.log("end")
+
+/*
+Output
+======
+start
+1
+3
+end
+
+reason: promise is not resolved so then block will not executed.
+*/
+
+
+// What is the output
+// console.log("start")
+
+// const fn = () => 
+//     new Promise((resolve, reject) => {
+//         console.log(1);
+//         resolve("success");
+//     })
+
+// console.log("middle")
+
+// fn().then(res => {
+//     console.log(res)
+// })
+
+// console.log("end")
+
+/*
+Output
+start
+middle
+1
+end
+success
+
+reason -> then always run after sync code.
+*/
